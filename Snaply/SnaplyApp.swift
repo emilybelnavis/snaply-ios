ec524545ccd20b9fdbfcc81580d48bad5281cf4b
Emily Belnavis <emily@emilybelnavis.ca>
2023-09-05 13:52:24 -0400

    Initial Commit

---
+++ Snaply/SnaplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnaplyApp.swift
+//  Snaply
+//
+//  Created by Samantha Emily-Rachel Belnavis on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct SnaplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
